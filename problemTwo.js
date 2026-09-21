e74f0371da9f14fc7c8be1549f055c0fc8de385a
Farid-256 <faridiqubal334@gmail.com>
2026-09-21 15:54:50 +0600

    problem 7 solve

--- problemTwo.js
+++ problemTwo.js
@@ -6,6 +6,22 @@ const reverseString = (str) =>{
 
  
 // Problem 7: Count Vowels in a String [Easy] Description: Write a function countVowels(str) that counts and returns the number of vowels (a, e, i, o, u) in a string. Example: Input: 'hello' → Output: 2Input: 'javascript' → Output: 3 Hint: Use a loop or match() with a regular expression.
-const countVowels = () =>{
+const countVowels = (str) =>{
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0
+    const lowerCase = str.toLowerCase()
 
+    for(let letter of lowerCase){
+        if(vowels.includes(letter)){
+            count++
+        }
+    }
+    return count
 }
+// console.log(countVowels('JavaScript'))
+
+// Problem 8: Check Palindrome  [Easy]
+// Description: Write a function isPalindrome(str) that returns true if the string reads the same forwards and backwards.
+// Example:
+// Input: 'racecar'  → Output: trueInput: 'hello'    → Output: false
+// Hint: Compare the string to its reverse.
